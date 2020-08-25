b943b576b28fbed89a591de45dabf25971f51914
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-08-25 14:13:02 +0300

    Add initial support for streaming videos

--- packages/server/src/dev_help/server.py
+++ packages/server/src/dev_help/server.py
@@ -1,9 +1,23 @@
+import aiofiles
 from starlette.applications import Starlette
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, FileResponse, StreamingResponse
 from starlette.routing import Route
 import uvicorn
+from pathlib import Path
+
+
+async def file_gen(file_name, blocksize=1024 * 256):
+    async with aiofiles.open(file_name, mode="rb") as file_like:
+        chunk = await file_like.read(blocksize)
+        while chunk:
+            yield chunk
+            chunk = await file_like.read(blocksize)
+
+async def video(request):
+    videopath = Path(__file__).parent.parent.parent / "data" / "edgedb_python.mp4"
+    return StreamingResponse(file_gen(str(videopath)), media_type="video/mp4")
 
 async def homepage(request):
     return JSONResponse({'hello': 'world'})
@@ -14,6 +28,7 @@ middleware = [
 
 app = Starlette(debug=True, routes=[
     Route('/', homepage),
+    Route('/video', video),
 ], middleware=middleware)
 
 def main():
