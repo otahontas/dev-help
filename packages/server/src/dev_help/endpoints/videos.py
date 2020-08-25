26532acc84089f32f63459b6a17a530deac45845
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-08-25 16:19:46 +0300

    Add support for uploading videos

--- packages/server/src/dev_help/endpoints/videos.py
+++ packages/server/src/dev_help/endpoints/videos.py
@@ -1,18 +1,42 @@
 from starlette.endpoints import HTTPEndpoint
-from starlette.responses import PlainTextResponse
+from starlette.requests import Request
+from starlette.responses import JSONResponse, StreamingResponse
+from starlette.exceptions import HTTPException
 from dev_help import settings
+import aiofiles
 
 class Videos(HTTPEndpoint):
-    async def get(self, request):
-        return PlainTextResponse(f"{settings.VIDEOS_PATH}")
+    """HTTP methods for API endpoint /videos."""
+    
+    async def get(self, request: Request) -> JSONResponse:
+        """Get filenames for all videos."""
+        files = [{"filename": path.name } for path in settings.VIDEOS_PATH.iterdir()]
+        return JSONResponse(files)
 
-# async def file_gen(file_name, blocksize=1024 * 256):
-#     async with aiofiles.open(file_name, mode="rb") as file_like:
-#         chunk = await file_like.read(blocksize)
-#         while chunk:
-#             yield chunk
-#             chunk = await file_like.read(blocksize)
-#
-# async def video(request):
-#     videopath = Path(__file__).parent.parent.parent / "data" / "edgedb_python.mp4"
-#     return StreamingResponse(file_gen(str(videopath)), media_type="video/mp4")
+    async def post(self, request: Request) -> JSONResponse:
+        """Add new video."""
+        form = await request.form()
+        filename = form["upload_video"].filename
+        contents = await form["upload_video"].read()
+        async with aiofiles.open(f"{settings.VIDEOS_PATH}/{filename}", 'wb') as new_video:
+            written_bytes = await new_video.write(contents)
+            if written_bytes != len(contents):
+                raise HTTPException(status_code=400, contents="Couldn't save video file to server.")
+        return JSONResponse({"filename": filename}, status_code=201)
+    
+class Video(HTTPEndpoint):
+    """HTTP methods for API endpoint /videos/{filename}."""
+
+    async def get(self, request: Request) -> StreamingResponse:
+        """Get single video as streamed response"""
+        filename = request.path_params['filename']
+        path_to_video = settings.VIDEOS_PATH / filename
+        if not path_to_video:
+            raise HTTPException(status_code=404)
+
+        async def video_generator(path_to_file: str, blocksize: int = 1024 * 256):
+            async with aiofiles.open(path_to_file, mode="rb") as file:
+                while chunk := await file.read(blocksize):
+                    yield chunk
+
+        return StreamingResponse(video_generator(str(path_to_video)), media_type="video/mp4")
--- packages/server/src/dev_help/server.py
+++ packages/server/src/dev_help/server.py
@@ -2,11 +2,9 @@ from starlette.applications import Starlette
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse, FileResponse, StreamingResponse
-=======
 from starlette.responses import JSONResponse
->>>>>>> 5304b64... Move video api endpoint definitions to Videos class
 from starlette.routing import Route
-from dev_help.endpoints.videos import Videos
+from dev_help.endpoints.videos import Videos, Video
 
 async def homepage(request):
     return JSONResponse({'hello': 'world'})
@@ -18,4 +16,5 @@ middleware = [
 app = Starlette(debug=True, routes=[
     Route('/', homepage),
     Route('/videos', Videos),
+    Route('/videos/{filename}', Video)
 ], middleware=middleware)
